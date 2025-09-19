5f37f0bfcdd29ac216e5f7c838b45c2b3c37047c
thande788 <thande.murito@gmail.com>
2025-09-20 01:26:16 +0300

    Update vite.config.js
    adding netlify dev server

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+server.allowedHosts { 'devserver-main--angel-touch.netlify.app' }
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
